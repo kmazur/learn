bf646a56af0ccd1e352d26e7196e04f709c8114f
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2017-02-22 00:04:29 +0100

    Format hackerrank 01 in w39

--- src/main/scala/com/hackerrank/contests/w29/Challenge01DayOfTheProgrammer.scala
+++ src/main/scala/com/hackerrank/contests/w29/Challenge01DayOfTheProgrammer.scala
@@ -2,6 +2,14 @@ package com.hackerrank.contests.w29
 
 object Solution {
 
+  val programmerDay = 256;
+
+  def getYearDays(year: Int): Array[Int] = {
+    val isLeap = if(year > 1918) { isLeapGregorian(_) } else { isLeapJulian(_) }
+    val leapFebruary = if(isLeap(year)) {29} else {28}
+    return Array(31, leapFebruary, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30);
+  }
+
   def isLeapJulian(year: Int): Boolean = {
     return year % 4 == 0
   }
@@ -10,38 +18,27 @@ object Solution {
     return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
   }
 
-  def printDate(isLeap: (Int) => Boolean, year: Int): Unit = {
-    val day = 256;
-    val leapFebruary = if (isLeap(year)) {
-      29
-    } else {
-      28
-    }
-    val days = Array(31, leapFebruary, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30);
+  def printDate(year: Int): Unit = {
+    val monthDays = getYearDays(year)
     var month = 1;
     var nth = 0;
-    var i = 0;
-    while (nth < day) {
-      nth += days(i);
+    while(nth < programmerDay) {
+      nth += monthDays(month - 1);
       month += 1;
-      i += 1;
     }
     month -= 1;
-    i -= 1;
-    val monthDiff = nth - day;
-    val monthDay = days(i) - monthDiff - 1;
+    val monthDiff = nth - programmerDay;
+    val monthDay = monthDays(month - 1) - monthDiff - 1;
     println(f"${monthDay}%02d.${month}%02d.${year}%04d");
   }
 
   def main(args: Array[String]) {
-    val sc = new java.util.Scanner(System.in);
+    val sc = new java.util.Scanner (System.in);
     val y = sc.nextInt();
-    if (y == 1918) {
+    if(y == 1918) {
       println("27.08.1918")
-    } else if (y > 1918) {
-      printDate(isLeapGregorian, y)
     } else {
-      printDate(isLeapJulian, y)
+      printDate(y)
     }
   }
 }
