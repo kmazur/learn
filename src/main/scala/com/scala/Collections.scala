619bcb1753578b07b4958447d5f5338a0df1d051
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2017-02-25 20:20:26 +0100

    Collections - part 2

--- src/main/scala/com/scala/Collections.scala
+++ src/main/scala/com/scala/Collections.scala
@@ -1,5 +1,6 @@
 package com.scala
 
+import scala.collection.immutable.Seq
 import scala.collection.mutable
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
@@ -75,7 +76,51 @@ object Collections {
     list :+ 100 // append  at the end
     100 +: list // prepend at the beginning
 
+    val head: Int = list.head
+    val headOption: Option[Int] = list.headOption
+    list.last
+    list.lastOption
+    list.isEmpty
+    list.nonEmpty
+    // map, flatMap, foreach, reduce[Left/Right], fold[Left, Firght]
+    // collect -> partial function
+    list.collect({ case i: Int => i})
+    // list count/forall/exists (predicate)
+    // list filter/filterNot/partition
+    // list takeWhile/dropWhile, take(n), drop(n), splitAt(n), takeRight(n), dropRight(n)
+    // list slice(from, to)
+    // list view(from, to)
+    val zip: Seq[(Int, String)] = List(1, 2, 3).zip(List("a", "b", "c"))
+    val index: Seq[(String, Int)] = List("a", "b", "c").zipWithIndex
+    List(1, 2, 3).zipAll(List("a", "b"), 0, "0")    // (1, "a"), (2, "b"), (3, "0")
+    List(1, 2).zipAll(List("a", "b", "c"), 0, "0")  // (1, "a"), (2, "b"), (0, "c")
+
+    List(1, 2, 3).grouped(2) // [[1, 2], [3]]
+    List(1, 2, 3).sliding(2) // [[1, 2], [2, 3]]
+    List(1, 2, 3).groupBy(i => i)
+    List(1, 2, 3).mkString(",")
+    list.toIndexedSeq.toIterable.toList.toBuffer.toStream.toSet.toVector
 
 
+    val ints = ArrayBuffer(1, 2, 3)
+    // transform is like map but mutates the collection
+    ints.transform(_ * 2)
+    println(ints) // 2, 4, 6
+
+
+    // foldLeft(0)(_ + _)
+    (0 /: List(1, 2, 3))(_ + _)
+
+    // foldRight(0)(_ + _)
+    (List(1, 2, 3) :\ 0)(_ + _)
+
+    val results: Seq[Int] = List(1, 2, 3).scanLeft(0)(_ + _)
+    println(results) // List(0, 1, 3, 6)
+
+
+
+    val list10 = (1 to 10).toBuffer
+    list10.view(0, 5).filter(_ % 2 == 0)
+    println(list10) // 1, 2, 3..., 10
   }
 }
