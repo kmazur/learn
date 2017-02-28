a55bf2bd271b6f0d5a7224fe36caeceb24a5ab44
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2017-02-28 22:37:16 +0100

    XmlProcessing

---
+++ src/main/scala/com/scala/XmlProcessing.scala
@@ -0,0 +1,40 @@
+package com.scala
+
+object XmlProcessing {
+
+  def main(args: Array[String]): Unit = {
+/*
+    // for inline syntax the scala-xml must be on the classpath. E.g.:
+
+    val doc = <html>
+      <head>
+        <title>Fred's Memoirs</title>
+      </head> <body>...</body>
+    </html>
+    println(doc.getClass)
+
+    val multipleNodes = <span>First</span> <span>second</span> <customNode>test</customNode>
+
+    val (x, y) = (1, 2)
+    // eror -> interpreted as opening xml element
+    // x <y
+    // ok
+    x < y
+
+
+    val items = new NodeBuffer
+    items += <li>Fred</li>
+    items += <li>Wilma</li>
+    val nodes: NodeSeq = items
+
+
+    val elem = <a href="http://scala-lang.org">The Scala language</a>
+    val url = elem.attributes("href").text
+
+    for (attr <- elem.attributes)
+      println(s"${attr.key} and ${attr.value.text}")
+
+    val map: Map[String, String] = elem.attributes.asAttrMap*/
+  }
+
+}
